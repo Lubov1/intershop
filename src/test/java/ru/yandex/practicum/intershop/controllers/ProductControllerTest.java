4fec12817676130b0f1c2974c09ab14c3d348328
Lubov1 <lubov.88867@gmail.com>
2025-06-03 02:26:15 +0200

    test added

--- src/test/java/ru/yandex/practicum/intershop/controllers/ProductControllerTest.java
+++ src/test/java/ru/yandex/practicum/intershop/controllers/ProductControllerTest.java
@@ -2,6 +2,12 @@ package ru.yandex.practicum.intershop.controllers;
 
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.util.MultiValueMap;
+import ru.yandex.practicum.intershop.dto.ItemDto;
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,6 +27,24 @@ class ProductControllerTest extends ControllerTest {
                     assertTrue(body.contains("10.02"));
                     assertTrue(body.contains("<span>0</span>"));
 
+                });
+    }
+    @Test
+    void getProducts() throws Exception {
+        webTestClient.get().uri("/main")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType("text/html")
+                .expectBody(String.class).consumeWith(response -> {
+                    String body = response.getResponseBody();
+                    assertNotNull(body);
+                    assertTrue(body.contains("<form"));
+                    assertTrue(body.contains("Первый продукт"));
+                    assertTrue(body.contains("Второй продукт"));
+                    assertTrue(body.contains("Третий продукт"));
+                    assertTrue(body.contains("Четвертый продукт"));
+                    assertTrue(body.contains("Пятый продукт"));
+                    assertFalse(body.contains("Шестой продукт"));
                 });
     }
 }
