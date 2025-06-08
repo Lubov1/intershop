05264abff1fa017d7b1e397079cfe5b102cc3684
Lubov1 <lubov.88867@gmail.com>
2025-06-08 15:42:19 +0200

    refactoring

--- payment-service/src/main/java/ru/yandex/practicum/paymentservice/controllers/BuyControllersImpl.java
+++ payment-service/src/main/java/ru/yandex/practicum/paymentservice/controllers/BuyControllersImpl.java
@@ -1,10 +1,12 @@
-package ru.yandex.practicum.paymentservice;
+package ru.yandex.practicum.paymentservice.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+import ru.yandex.practicum.paymentservice.services.BalanceService;
 import ru.yandex.practicum.paymentservice.api.BuyApi;
 import ru.yandex.practicum.paymentservice.controllers.domain.Balance;
 
@@ -15,7 +17,7 @@ public class BuyControllersImpl implements BuyApi {
     @Autowired
     public BalanceService balanceService;
     @Override
-    public Mono<ResponseEntity<Balance>> buyPost(BigDecimal price, ServerWebExchange exchange) {
+    public Mono<ResponseEntity<Balance>> buyPost(@RequestParam BigDecimal price, ServerWebExchange exchange) {
         BigDecimal balance = balanceService.getBalance().subtract(price);
         if (balance.compareTo(BigDecimal.ZERO) >= 0) {
             balanceService.setBalance(balance);
--- payment-service/src/main/java/ru/yandex/practicum/paymentservice/services/BalanceService.java
+++ payment-service/src/main/java/ru/yandex/practicum/paymentservice/services/BalanceService.java
@@ -1,6 +1,5 @@
-package ru.yandex.practicum.paymentservice;
+package ru.yandex.practicum.paymentservice.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
